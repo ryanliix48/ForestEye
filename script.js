f35374ba68589fcdbdf2972ad91093eb32e54366
ryanliix48 <oil14659@gmail.com>
2026-07-04 15:41:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+window.addEventListener("scroll", function () {
 
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 50) {
+
+        header.style.background = "rgba(0,0,0,0.8)";
+        header.style.backdropFilter = "blur(20px)";
+        header.style.transition = ".4s";
+
+    } else {
+
+        header.style.background = "rgba(0,0,0,.45)";
+
+    }
+
+});
